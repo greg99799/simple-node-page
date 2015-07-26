332897e81933bde2dbdfeab29e6a6b33237519e0
Greg <greg.knudsen09@gmail.com>
2015-07-25 20:26:42 -0400

    add pseudo code as per chalkers

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-//Problem:
-//Solution:
+//Problem: We need a simple way to look at a user's badge count and Javascript points from a web browser
+//Solution: Use Node.js to perform the profile look ups and serve our templates via HTTP
+
+//1. Create a web server
+
+//2. Handle HTTP route GET / and POST / i.e. Home 
+  //if url = "/" && GET
+    //show search 
+  //if url = "/" && POST
+    //redirect to /:username
+
+//3. Handle HTTP route for GET /:username i.e. /greg99799
+  //if url = "/..."
+    //get json from Treehouse
+      //on "end"
+        //show profile
+      //on "error"
+        //show error
+
+//4. Function that handle the reading of files and merge in values
+  //read from file and get string
+    //merge values into string
