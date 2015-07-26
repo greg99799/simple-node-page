59780422d73e84b8efbd99c384ff6d082e927088
Greg <greg.knudsen09@gmail.com>
2015-07-26 13:50:57 -0400

    using writeHead, the user is redirected to the proper username stat page

--- router.js
+++ router.js
@@ -21,9 +21,9 @@ function home(request, response) {
     request.on("data", function(postBody) {
       //extract username
       var query = querystring.parse(postBody.toString());
-      response.write(query.username);
+      //redirect to /:username
+      response.writeHead(303, {"Location": "/" + query.username});
       response.end();
-        //redirect to /:username
     });
       
     }
