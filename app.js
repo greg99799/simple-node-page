78c0ae9743f56a620ff70699028b3f9fa1bb7820
Greg <greg.knudsen09@gmail.com>
2015-07-25 20:42:15 -0400

    learn that once 'end' method is called, nothing more can be called to response - JS asynchronous nature at work

--- app.js
+++ app.js
@@ -6,7 +6,10 @@
 var http = require('http');
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+  setInterval(function(){
+  response.write(new Date() + "\n");
+  }, 1000);
+  //response.end('Hello World\n');
 }).listen(8000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8000/');
 
