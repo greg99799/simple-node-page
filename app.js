18f25c5018afb31e31be96ff18812c388f80623f
Greg <greg.knudsen09@gmail.com>
2015-07-25 20:57:25 -0400

    create home and user routes

--- app.js
+++ app.js
@@ -5,20 +5,23 @@
 
 var http = require('http');
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  setInterval(function(){
-  response.write(new Date() + "\n");
-  }, 1000);
-  //response.end('Hello World\n');
+  homeRoute(request, response);
 }).listen(8000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8000/');
 
 //2. Handle HTTP route GET / and POST / i.e. Home 
+function homeRoute(request, response) {
   //if url = "/" && GET
+  if (request.url === "/") {
     //show search 
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write("Header\n");
+    response.write("Search\n");
+    response.end("Footer\n");
+  }
   //if url = "/" && POST
     //redirect to /:username
-
+}
 //3. Handle HTTP route for GET /:username i.e. /greg99799
   //if url = "/..."
     //get json from Treehouse
